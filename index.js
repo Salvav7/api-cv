3ef0ffa37dab310e5123bb1c15d43b5174f0c29e
Salvav <villegassalva94@gmail.com>
2023-09-14 19:52:22 -0600

    api cv

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const cors = require('cors')
-const express = require('express')
-
+const cors = require("cors");
+const express = require("express");
 
 const app = express();
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 const port = 8080;
 
-app.get('/', (req, res, next) => {
-	res.send('node express api <br> by salvav');
+app.get("/", (req, res, next) => {
+  res.send("node express api <br> by salvav");
 });
 
-
-app.get('/header', (req, res, next) => {
+app.get("/header", (req, res, next) => {
   res.sendfile("assets/header.json");
 });
 
-app.listen(port,  () => 
-	console.log('listening on port ' + port
-));
+app.get("/work-experience", (req, res, next) => {
+  res.sendfile("assets/work-experience.json");
+});
+
+app.get("/skills", (req, res, next) => {
+  res.sendfile("assets/skills.json");
+});
+
+app.get("/certifications", (req, res, next) => {
+  res.sendfile("assets/certifications.json");
+});
+
+
+app.get("/interests", (req, res, next) => {
+  res.sendfile("assets/interests.json");
+});
+
+app.listen(port, () => console.log("listening on port " + port));
